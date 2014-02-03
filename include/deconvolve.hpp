20262c06e7c11e0695f4ebbbff32c21b1313f436
Alex Fix <afix@cs.cornell.edu>
2014-02-03 16:17:12 -0500

    Adding declaration for deconvolve

---
+++ include/deconvolve.hpp
@@ -0,0 +1,27 @@
+#ifndef _DECONVOLVE_HPP_
+#define _DECONVOLVE_HPP_
+
+#include <functional>
+#include <boost/multi_array.hpp>
+
+namespace deconvolution {
+
+template <int D>
+using Array = boost::multi_array<double, D>;
+template <int D>
+using LinearSystem = std::function<void(const Array<D>&, Array<D>&)>;
+
+template <int D> class Regularizer;
+
+/* 
+ * Solve a linear inverse system of the form
+ * min_x |y - Hx|_2^2 + R(x)
+ * where H is a linear system, R is a regularizer, and y are some given 
+ * observables. 
+ */
+template <int D>
+Array<D> Deconvolve(const Array<D>& y, const LinearSystem<D>& H, const LinearSystem<D>& Q, const Regularizer<D>& R);
+
+}
+
+#endif
---
+++ src/deconvolve.cpp
@@ -0,0 +1,15 @@
+#include "deconvolve.hpp"
+
+namespace deconvolution{
+template <int D>
+Array<D> Deconvolve(const Array<D>& y, const LinearSystem<D>& H, const LinearSystem<D>& Q, const Regularizer<D>& R) {
+    return {};
+}
+
+#define INSTANTIATE_DECONVOLVE(d) \
+    template Array<d> Deconvolve<d>(const Array<d>& y, const LinearSystem<d>& H, const LinearSystem<d>& Q, const Regularizer<d>& R);
+INSTANTIATE_DECONVOLVE(1)
+INSTANTIATE_DECONVOLVE(2)
+INSTANTIATE_DECONVOLVE(3)
+#undef INSTANTIATE_DECONVOLVE
+}
