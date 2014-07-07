84f472559936b156475251af4329672dfbd02ff3
Charles Herrmann <irwinherrmann@gmail.com>
2014-07-07 13:25:44 -0400

    weighted least square

---
+++ medi/MEDI_L1.m
@@ -0,0 +1,60 @@
+% Morphology Enabled Dipole Inversion (MEDI)
+%   [x, cost_reg_history, cost_data_history] = MEDI_L1(varargin)
+%
+%   output
+%   x - the susceptibility distribution 
+%   cost_reg_history - the cost of the regularization term
+%   cost_data_history - the cost of the data fidelity term
+%   
+%   input
+%   RDF.mat has to be in current folder.  
+%   MEDI_L1('lambda',lam,...) - lam specifies the regularization parameter
+%                               lam is in front of the data fidelity term
+%
+
+function [x, cost_reg_history, cost_data_history] = MEDI_L1(varargin)
+
+[lambda iFreq RDF N_std iMag Mask matrix_size matrix_size0 voxel_size delta_TE CF B0_dir merit smv radius data_weighting gradient_weighting Debug_Mode] = parse_QSM_input(varargin{:});
+
+
+%%%%%%%%%%%%%%% weights definition %%%%%%%%%%%%%%
+cg_max_iter = 100;
+cg_tol = 0.01;
+max_iter = 10;
+tol_norm_ratio = 0.1;
+data_weighting_mode = data_weighting;
+gradient_weighting_mode = gradient_weighting;
+
+tempn = double(N_std);
+D=dipole_kernel(matrix_size, voxel_size, B0_dir);
+
+div = @cdiv;
+
+%% IRLS settings
+grad = @(arg) cgrad(arg, voxel_size);
+solve = @(A, b) real(cgsolve(A, b, cg_tol, cg_max_iter, 0));
+H  = @(arg) m.*(real(ifftn(D.*fftn(arg))));
+
+x = zeros(prod(matrix_size))
+m = reshape(iMag, size(x))
+rdf = reshape(RDF, size(x))
+p = .7
+
+A = @(x) [(rdf-H(x)).^2, grad(x).^2, (grad(x)-grad(m)).^2]
+
+%% IRLS
+QSM_IRLS(A, solve, x, p, K, KK)
+
+%convert x to ppm
+x = x/(2*pi*delta_TE*CF)*1e6.*Mask;
+
+store_QSM_results(x, iMag, RDF, Mask,...
+                  'Norm', 'L1','Method','MEDIN','Lambda',lambda,...
+                  'SMV',smv,'Radius',radius,'IRLS',merit,...
+                  'voxel_size',voxel_size,'matrix_size',matrix_size,...
+                  'Data_weighting_mode',data_weighting_mode,'Gradient_weighting_mode',gradient_weighting_mode,...  
+                  'L1_tol_ratio',tol_norm_ratio, 'Niter',iter,...
+                  'CG_tol',cg_tol,'CG_max_iter',cg_max_iter,...
+                  'B0_dir', B0_dir);
+
+end
---
+++ medi/wcgsolve.m
@@ -0,0 +1,70 @@
+% wcgsolve.m
+%
+% Solve a symmetric positive definite system Ax = b with weights via conjugate gradients.
+
+function [x, res, iter] = wcgsolve(A, b, w, tol, maxiter, verbose, x0)
+
+matrix_size=size(b);
+b=b(:);
+
+if (nargin < 6)
+    x = zeros(length(b),1);
+else 
+    x=x0(:);
+end
+
+if (nargin < 5), verbose = 1; end
+
+implicit = isa(A,'function_handle');
+
+if (nargin < 6)
+    r = b;
+else
+    if (implicit), r = b - w*A(reshape(x,matrix_size)); r=r(:);  else, r = w*(b - A*x);  end
+end
+d = r;
+delta = r'*r;
+delta0 = b'*b;
+numiter = 0;
+bestx = x;
+bestres = sqrt(delta/delta0); 
+while ((numiter < maxiter) & (delta > tol^2*delta0))
+
+  % q = A*d
+  if (implicit), q = w*A(reshape(d,matrix_size)); q=q(:);  else, q = w*A*d;  end
+ 
+  alpha = delta/(d'*q);
+  x = x + alpha*d;
+  
+  if (mod(numiter+1,50) == 0)
+    % r = b - Aux*x
+    if (implicit), r = w*b - w*reshape(A(reshape(x,matrix_size)),size(b));  else, r = w*b - w*A*x;  end
+  else
+    r = r - alpha*q;
+  end
+  
+  deltaold = delta;
+  delta = r'*r;
+  beta = delta/deltaold;
+  d = r + beta*d;
+  numiter = numiter + 1;
+  if (sqrt(delta/delta0) < bestres)
+    bestx = x;
+    bestres = sqrt(delta/delta0);
+  end    
+  
+  if ((verbose) & (mod(numiter,verbose)==0))
+    disp(sprintf('cg: Iter = %d, Best residual = %8.3e, Current residual = %8.3e', ...
+      numiter, bestres, sqrt(delta/delta0)));
+  end
+  
+end
+
+if (verbose)
+  disp(sprintf('cg: Iterations = %d, best residual = %14.8e', numiter, bestres));
+end
+% x = reshape(bestx,matrix_size);
+x = reshape(x,matrix_size);
+res = bestres;
+iter = numiter;
+
