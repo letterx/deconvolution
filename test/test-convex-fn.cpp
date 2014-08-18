40bc09798619a77e1fa621b0a13d23f7e75eb681
Alex Fix <afix@cs.cornell.edu>
2014-08-17 20:01:08 -0400

    Adding tests for moreauEnvelope and moreauGrad

--- test/test-convex-fn.cpp
+++ test/test-convex-fn.cpp
@@ -49,6 +49,42 @@ BOOST_AUTO_TEST_SUITE(TestConvexFn)
             BOOST_CHECK_EQUAL(f(3.1), std::numeric_limits<double>::max());
         }
 
+        BOOST_AUTO_TEST_CASE(Moreau) {
+            const double t = 0.1;
+
+            BOOST_CHECK_CLOSE(f.moreauEnvelope(-2.00, t), 7.0   , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope(-1.16, t), 2.128 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope(-1.15, t), 2.1125, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope(-1.00, t), 1.8875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 0.00, t), 0.3875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 0.85, t),-0.8875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 1.00, t),-1.0   , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 1.25, t),-0.6875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 2.00, t), 1.1875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 3.00, t), 3.6875, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 3.25, t), 4.3125, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 3.26, t), 4.338 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauEnvelope( 4.00, t), 9.0   , epsilon);
+        }
+
+        BOOST_AUTO_TEST_CASE(MoreauGrad) {
+            const double t = 0.1;
+
+            BOOST_CHECK_CLOSE(f.moreauGrad(-2.00, t),-10.0, epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad(-1.16, t),-1.6 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad(-1.15, t),-1.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad(-1.00, t),-1.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 0.00, t),-1.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 0.85, t),-1.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 1.00, t), 0.0 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 1.25, t), 2.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 2.00, t), 2.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 3.00, t), 2.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 3.25, t), 2.5 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 3.26, t), 2.6 , epsilon);
+            BOOST_CHECK_CLOSE(f.moreauGrad( 4.00, t), 10.0, epsilon);
+        }
+
     BOOST_AUTO_TEST_SUITE_END()
 
 
